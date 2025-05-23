a587b2731058b2aaaf9e3713ec240907c9c0926e
Dipto Roy <diptoray292@gmail.com>
2025-05-23 20:38:32 +0600

    create jwt authentication verify

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 let cors = require('cors');
+let cookieParser = require('cookie-parser');
+app.use(cookieParser());
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
@@ -8,6 +10,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(express.json());
+let jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 let port = process.env.PORT || 5000;
@@ -27,13 +30,43 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        let verifyToken = (req, res, next) => {
+            const token = req.cookies?.token;
+            if (!token) {
+                return res.status(401).send({
+                    message: 'unauthorized access'
+                })
+            }
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decode) => {
+                if (error) {
+                    return res.status(401).send({
+                        message: 'forbidden access'
+                    })
+                }
+
+                req.decoded = decode;
+                next();
+            })
+        }
+
         let studentsCollection = client.db('School-Sphere').collection('students');
 
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        app.post('/jwt', (req, res) => {
+            let user = req.body;
+            let token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            }).send({ token });
+        })
 
-        app.get('/students', async (req, res) => {
+        app.get('/students', verifyToken, async (req, res) => {
             const page = parseInt(req.query.page) || 0;
             const size = parseInt(req.query.size) || 10;
             const gender = req.query.category;
@@ -62,7 +95,7 @@ async function run() {
         });
 
 
-        app.get('/countStudents', async (req, res) => {
+        app.get('/countStudents', verifyToken, async (req, res) => {
             let gender = req.query.gender;
             let className = req.query.className;
             let query = {};
